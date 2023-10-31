d104c1fbd196ce186609dd6522845cc46e8d7b1b
Alice Gerasimenko <alicegerasimenko@gmail.com>
2023-10-31 19:52:15 +0300

    [feature/SI-6285] Navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'newScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      routes: {
+        '/second': (context) => NewScreen(),
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -31,13 +36,42 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  late String data = '';
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            data = await Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const NewScreen()),
+            );
+            setState(() {});
+          },
+          child: Text('tap $data'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            '/second',
+            arguments: SecondScreenArguments('new Screen'),
+          );
+        },
+        child: const Icon(Icons.add_circle),
+      ),
     );
   }
+}
+
+class SecondScreenArguments {
+  final String text;
+
+  SecondScreenArguments(this.text);
 }
